ee7d7fba1fe39e0c260a4c6fb1ce17bb0fbcb177
acarlton199 <acarlton199@live.com>
2016-05-04 20:25:43 -0600

    Created Player and Dice

---
+++ diceRoller/diceRoller/Dice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace diceRoller
+{
+    class Dice
+    {
+        Random r;
+        public int roll;
+
+        public int diceRoll()
+        {
+            r = new Random();
+            roll = r.Next(1, 6) + r.Next(1,6); 
+            return roll;
+        }
+    }
+}
---
+++ diceRoller/diceRoller/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace diceRoller
+{
+    class Player
+    {
+        public int CurrentWins { get; set; }
+        public int CurrentLosses { get; set; }
+        public int CurrentRoll { get; set; }
+
+        public Player()
+        {
+            CurrentWins = 0;
+            CurrentLosses = 0;
+        }
+    }
+}
